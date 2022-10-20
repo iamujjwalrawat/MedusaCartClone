4ecd60180ba61db5d86ecef4677d03185719890d
Ujjwal Rawat <108294627+iamujjwalrawat@users.noreply.github.com>
2022-10-21 03:14:41 +0530

    Add files via upload

---
+++ MedusaCartClone/MedusaCartClone/MedusaCartCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedusaCartCloneApp.swift
+//  MedusaCartClone
+//
+//  Created by Ujjwal Rawat on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MedusaCartCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
